e45411ffe3d8af0edbc858405c58dec125f94f4c
ngugimukanga <ngugimukanga@gmail.com>
2023-05-22 14:46:46 +0300

    register form validation and store

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -47,12 +47,32 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
+
+    public function showRegistrationForm()
+    {
+        $data = [];
+        $data['genders'] = \DB::table('genders')->get();
+        $data['courses'] = \DB::table('courses')->get();
+        $data['education_levels'] = \DB::table('education_level')->get();
+        $data['marital_statuses'] = \DB::table('marital_statuses')->get();
+        $data['preferred_times'] = \DB::table('preferred_time_of_class')->get();
+        $data['methods'] = \DB::table('preferred_time_of_class')->get();
+        return view('auth.register', $data);
+    }
     protected function validator(array $data)
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'dob' => ['required'],
+            'gender_id' => ['required'],
+            'location' => ['required', 'string', 'max:255'],
+            'marrital_status_id' => ['required'],
+            'phone_number' => ['required', 'max:10', 'min:10'],
+            'course_id' => ['required', 'max:10', 'min:10', 'unique:users'],
+            'preferred_time_of_class_id' => ['required'],
+            'how_you_learnt_about_us_id' => ['required'],
         ]);
     }
 
@@ -64,10 +84,27 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $numbers = '123456789';
+        $alphabets = 'ABCDEFGHIJKLMNPQRSTUVWXYZ';
+
+        $ref_number = substr(str_shuffle($alphabets), 0, 3) . substr(str_shuffle($numbers), 0, 3);
+        $user = [
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-        ]);
+            'dob' => $data['dob'],
+            'gender_id' => $data['gender_id'],
+            'marrital_status_id' => $data['marrital_status_id'],
+            'phone_number' => $data['phone_number'],
+            'location' => $data['location'],
+            'sub_location' => $data['sub_location'],
+            'national_id' => $data['national_id'],
+            'passport' => $data['passport'],
+            'course_id' => $data['course_id'],
+            'preferred_time_of_class_id' => $data['preferred_time_of_class_id'],
+            'how_you_learnt_about_us_id' => $data['how_you_learnt_about_us_id'],
+            'ref_number' => $ref_number,
+        ];
+        return User::create($user);
     }
 }
