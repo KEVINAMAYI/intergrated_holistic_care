21053eec6eca259fe796fd321207fef9c567ff09
KEVINAMAYI <kevinamayi20@gmail.com>
2023-05-23 11:25:37 +0300

    added dashboard

--- resources/views/frontend/layouts/partials/header.blade.php
+++ resources/views/frontend/layouts/partials/header.blade.php
@@ -51,7 +51,7 @@
                                             @endif
                                         @else
                                             <li class="nav-item dropdown">
-                                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#"
+                                                <a " id="navbarDropdown" class="nav-link dropdown-toggle" href="#"
                                                    role="button" data-bs-toggle="dropdown" aria-haspopup="true"
                                                    aria-expanded="false" v-pre>
                                                     {{ Auth::user()->name }}
@@ -59,7 +59,7 @@
 
                                                 <div class="dropdown-menu dropdown-menu-end"
                                                      aria-labelledby="navbarDropdown">
-                                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                                    <a id="logoutbtn" style="color:black; padding-left:10px;" class="dropdown-item" href="{{ route('logout') }}"
                                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                                         {{ __('Logout') }}
