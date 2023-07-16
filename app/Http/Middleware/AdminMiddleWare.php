b34724528f1756ddee58a54f7cb968b02f0fbb89
KEVINAMAYI <kevinamayi20@gmail.com>
2023-07-16 22:03:05 +0300

    [#ISSUE 40] Add ability to view, edit and delete Questions

--- app/Http/Middleware/AdminMiddleWare.php
+++ app/Http/Middleware/AdminMiddleWare.php
@@ -17,9 +17,9 @@ class AdminMiddleWare
      */
     public function handle(Request $request, Closure $next)
     {
-            if (Auth::user()->role->name != 'Admin') {
-                abort(403);
-            }
+        if (Auth::user()->role->name != 'Admin') {
+            abort(403);
+        }
 
          return $next($request);
     }
